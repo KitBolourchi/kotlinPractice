984c58f62efae801d3dcac6fec980af7a63961fc
kitbolourchi <74569356+KitBolourchi@users.noreply.github.com>
2023-05-24 08:37:16 +0100

    Stuff

--- src/main/kotlin/project/minesweeper.kt
+++ src/main/kotlin/project/minesweeper.kt
@@ -1,25 +1,36 @@
 package project
 
 fun main() {
-	createBoard()
-}
-
-fun createBoard() {
-	val m = MutableList(9) { MutableList(9) {". "} }
-	val numOfMines = 9
+	val m = createBoard()
+	addMinesToBoard(m,15)
 
 	for (i in 0 until m.size) {
 		for (j in 0 until m[i].size) {
-			val randNum = (0..8).random()
-			when (j) {
-				randNum -> m[i][j] = "X "
-			}
 			print(m[i][j])
 		}
 		println()
 	}
 }
 
-fun addMines(board : MutableList<Int>, numOfMines : Int) {
+fun createBoard(): MutableList<MutableList<String>> {
+	return MutableList(9) { MutableList(9) {". "}}
+}
+
+fun addMinesToBoard(board : MutableList<MutableList<String>>, num : Int) {
+	var numOfMines = num
+	while (numOfMines > 0) {
+		for (i in 0 until board.size) {
+			for (j in 0 until board.size) {
+				val randNum = (0..20).random()
+				if (randNum % 6 == 0 && numOfMines > 0 && board[i][j] == ". ") {
+					board[i][j] = "X "
+					numOfMines--
+				}
+			}
+		}
+	}
+}
+
+fun checkNeighbours() {
 
 }
