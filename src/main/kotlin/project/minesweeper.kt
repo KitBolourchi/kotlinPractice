88dde0a154c44f0754c9d3e37d88cad9545a38df
kitbolourchi <74569356+KitBolourchi@users.noreply.github.com>
2023-05-25 10:29:49 +0100

    Update

--- src/main/kotlin/project/minesweeper.kt
+++ src/main/kotlin/project/minesweeper.kt
@@ -3,6 +3,8 @@ package project
 fun main() {
 	val m = createBoard()
 	addMinesToBoard(m,15)
+	checkNeighbours(m)
+
 
 	for (i in 0 until m.size) {
 		for (j in 0 until m[i].size) {
@@ -13,24 +15,45 @@ fun main() {
 }
 
 fun createBoard(): MutableList<MutableList<String>> {
-	return MutableList(9) { MutableList(9) {". "}}
+	val board = MutableList(11) { MutableList(21) {". "}}
+	for (i in 0 until board.size) {
+		for (j in 0 until board[i].size) {
+			if (i == 0 || i == 11 || j == 0 || j == 21) {
+				board[i][j] = ""
+			}
+		}
+	}
+	return board
 }
 
 fun addMinesToBoard(board : MutableList<MutableList<String>>, num : Int) {
 	var numOfMines = num
 	while (numOfMines > 0) {
-		for (i in 0 until board.size) {
-			for (j in 0 until board.size) {
-				val randNum = (0..20).random()
-				if (randNum % 6 == 0 && numOfMines > 0 && board[i][j] == ". ") {
-					board[i][j] = "X "
-					numOfMines--
-				}
-			}
+		val randomCol = (1..10).random()
+		val randomRow = (1..20).random()
+		if (board[randomCol][randomRow] == ". ") {
+			board[randomCol][randomRow] = "X "
+			numOfMines--
 		}
 	}
 }
 
-fun checkNeighbours() {
+fun checkNeighbours(board : MutableList<MutableList<String>>) {
+	for (i in 1 until board.size-1) {
+		for (j in 1 until board[i].size-1) {
+			var numberOfLocalMines = 0
+			if (board[i][j] == ". ") {
+				if (board[i][j + 1] == "X ") numberOfLocalMines++
+				if (board[i + 1][j + 1] == "X ") numberOfLocalMines++
+				if (board[i + 1][j] == "X ") numberOfLocalMines++
+				if (board[i + 1][j - 1] == "X ") numberOfLocalMines++
+				if (board[i][j - 1] == "X ") numberOfLocalMines++
+				if (board[i - 1][j - 1] == "X ") numberOfLocalMines++
+				if (board[i - 1][j] == "X ") numberOfLocalMines++
+				if (board[i - 1][j + 1] == "X ") numberOfLocalMines++
+			}
+			if (numberOfLocalMines > 0) board[i][j] = "$numberOfLocalMines "
+		}
+	}
 
 }
